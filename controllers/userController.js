e87a93b148c4cafbaff44973d99435de292d43c0
Praful Kumar <praful.k180@gmail.com>
2024-03-24 14:57:04 +0530

    bug-fix: add fileFilter to multer
    fileFilter prevents uploading of non-image file

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,7 +23,12 @@ const filterObj = (obj, ...allowedFields) => {
   return newObj;
 };
 
-exports.upload = multer({ storage: storage });
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) cb(null, true);
+  else cb(new AppError('Not an image! Please upload only images.', 400), false);
+};
+
+exports.upload = multer({ storage: storage, fileFilter });
 
 exports.getMe = (req, res, next) => {
   req.params.id = req.user.id;
