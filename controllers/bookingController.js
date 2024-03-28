e4558b3c206d31a3a3995560a6eeaa207360539d
Praful Kumar <praful.k180@gmail.com>
2024-03-28 21:45:28 +0530

    feat: Add createBookingCheckout function
    - Implement createBookingCheckout function in the booking controller.
    - Create middleware in viewRoutes to handle booking checkout.

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -1,5 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const Tour = require('./../models/tourModel');
+const Booking = require('./../models/bookingModel');
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
 
@@ -10,7 +11,9 @@ exports.createCheckoutSession = catchAsync(async (req, res, next) => {
 
   const session = await stripe.checkout.sessions.create({
     mode: 'payment',
-    success_url: `${req.protocol}://${req.hostname}:${req.app.get('port')}/`,
+    success_url: `${req.protocol}://${req.hostname}:${req.app.get(
+      'port'
+    )}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
     cancel_url: `${req.protocol}://${req.hostname}:${req.app.get(
       'port'
     )}/tour/${tour.slug}`,
@@ -38,4 +41,14 @@ exports.createCheckoutSession = catchAsync(async (req, res, next) => {
     status: 'success',
     session
   });
+});
+
+exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+  // This is only TEMPORARY, because it's UNSECURE: everyone can make bookings without paying
+  const { tour, user, price } = req.query;
+
+  if (!tour && !user && !price) return next();
+  await Booking.create({ tour, user, price });
+
+  res.redirect(req.originalUrl.split('?')[0]);
 });
