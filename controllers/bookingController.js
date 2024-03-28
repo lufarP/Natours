19917983c3bea3deb763f82a43f9c76c9cb906f6
Praful Kumar <praful.k180@gmail.com>
2024-03-28 17:40:50 +0530

    feat(api): Integrate Stripe for booking checkout sessions
    - Implement backend routes and controllers for booking checkout sessions
    - Introduce Stripe integration to generate checkout sessions for tours
    - Update dependencies to include the Stripe package

---
+++ controllers/bookingController.js
@@ -0,0 +1,43 @@
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+const Tour = require('./../models/tourModel');
+const catchAsync = require('./../utils/catchAsync');
+const AppError = require('./../utils/appError');
+
+exports.createCheckoutSession = catchAsync(async (req, res, next) => {
+  const tour = await Tour.findById(req.params.tourId);
+
+  if (!tour) return next(new AppError('No tour found with that ID!', 400));
+
+  const session = await stripe.checkout.sessions.create({
+    mode: 'payment',
+    success_url: `${req.protocol}://${req.get('hostname')}:${req.app.get(
+      'port'
+    )}/`,
+    cancel_url: `${req.protocol}://${req.get('hostname')}:${req.app.get(
+      'port'
+    )}/tour/${tour.slug}`,
+    customer_email: req.user.email,
+    client_reference_id: req.params.tourId,
+    line_items: [
+      {
+        price_data: {
+          product_data: {
+            name: `${tour.name} Tour`,
+            description: tour.summary,
+
+            images: [`https://www.natours.dev/img/tours/${tour.imageCover}`]
+          },
+          unit_amount: tour.price * 100,
+          currency: 'usd'
+        },
+        quantity: 1
+      }
+    ]
+  });
+
+  // 3) Create session as response
+  res.status(200).json({
+    status: 'success',
+    session
+  });
+});
